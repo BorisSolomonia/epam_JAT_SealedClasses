28a20d951b77473bb4a9601aa23cacc60de771c0
Boris Solomonia <borissolomoniaphone@gmail.com>
2022-07-20 01:11:53 +0400

    second commit to master

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         Supporter freelancer_5 = new Freelancer("Boris","Georgia",32,'5');
-        Supporter financialSupporter_3 = new FinancialSupporter("Nino","Georgia",30,'3');
+        Supporter financialSupporter__3 = new FinancialSupporter("Nino","Georgia",30,'3');
     }
 
 }
